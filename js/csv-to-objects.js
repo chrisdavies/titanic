56e7537fe50ef4ecf717599ff956f2a2c8b2c0ec
Chris Davies <chris.davies@theironyard.com>
2015-05-19 17:35:25 -0400

    Adding more robust csv parsing

--- js/csv-to-objects.js
+++ js/csv-to-objects.js
@@ -1,6 +1,118 @@
+function Tokenizer(str) {
+  this.str = str;
+  this.tokenStart = 0;
+  this.index = 0;
+}
+
+Tokenizer.prototype = {
+  get char () {
+    return this.str[this.index];
+  },
+
+  get peek () {
+    return this.str[this.index + 1];
+  },
+
+  get EOF () {
+    return this.index >= this.str.length;
+  },
+
+  moveNext: function () {
+    return ++this.index < this.str.length;
+  },
+
+  nextIndexOf: function (fn) {
+    for (var i = this.index; i < this.str.length; ++i) {
+      if (fn(this.str[i])) {
+        return i;
+      }
+    }
+
+    return -1;
+  },
+
+  popChar: function () {
+    var ch = this.char;
+    this.tokenStart = ++this.index;
+    return ch;
+  },
+
+  popToken: function () {
+    var token = this.str.slice(this.tokenStart, this.index);
+    this.tokenStart = this.index;
+    return token;
+  }
+};
+
+function csvTo2DArray(tokenizer) {
+  return readRows(tokenizer);
+
+  function readRows(tokenizer) {
+    var rows = [];
+
+    while (!tokenizer.EOF) {
+      rows.push(readFields(tokenizer));
+    }
+
+    return rows;
+  }
+
+  function readFields(tokenizer) {
+    var fields = [];
+
+    while (!tokenizer.EOF) {
+      var field;
+
+      if (isQuotedField(tokenizer)) {
+        field = readQuotedField(tokenizer);
+      } else {
+        field = readUnquotedField(tokenizer);
+      }
+
+      fields.push(field.replace(/\"\"\"/g, '"'));
+
+      if (tokenizer.popChar() === '\n') {
+        break;
+      }
+    }
+
+    return fields;
+  }
+
+  function isQuotedField(tokenizer) {
+    // Find the index of the next non-quote character
+    var nonQuotedCharIndex = tokenizer.nextIndexOf(function (ch) {
+      return ch !== '"';
+    });
+
+    return tokenizer.char === '"' && ((nonQuotedCharIndex - tokenizer.index) % 3);
+  }
+
+  function readQuotedField(tokenizer) {
+    while (tokenizer.moveNext() && (tokenizer.char !== '"' || tokenizer.peek === '"')) {
+      if (tokenizer.char === '"') {
+        skipTripleQuotes(tokenizer);
+      }
+    }
+
+    tokenizer.moveNext();
+    return tokenizer.popToken().slice(1, -1);
+  }
+
+  function skipTripleQuotes(tokenizer) {
+    tokenizer.moveNext();
+    tokenizer.moveNext();
+  }
+
+  function readUnquotedField(tokenizer) {
+    while (tokenizer.moveNext() && tokenizer.char !== ',' && tokenizer.char !== '\n');
+
+    return tokenizer.popToken();
+  }
+}
+
 function csvToObjects(csv, propertyNames, ignoreFirstRecord) {
-  var records = csv.split('\n')
-    .map(sanitizeLine)
+  var records = csvTo2DArray(new Tokenizer(csv))
     .map(lineToObject);
 
   if (ignoreFirstRecord) {
@@ -9,8 +121,7 @@ function csvToObjects(csv, propertyNames, ignoreFirstRecord) {
     return records;
   }
 
-  function lineToObject(line) {
-    var fields = line.split(',');
+  function lineToObject(fields) {
     var obj = {};
 
     fields.forEach(function (field, index) {
@@ -22,26 +133,5 @@ function csvToObjects(csv, propertyNames, ignoreFirstRecord) {
     });
 
     return obj;
-  }
-
-  function sanitizeLine(line) {
-    var sanitizedStr = '';
-    var isInQuote = false;
-
-    for (var i = 0; i < line.length; ++i) {
-      var ch = line[i];
-
-      if (isInQuote && ch === '"') {
-        isInQuote = false;
-      } else if (ch === '"') {
-        isInQuote = true;
-      }
-
-      if (!isInQuote || ch !== ',') {
-        sanitizedStr += ch;
-      }
-    }
-
-    return sanitizedStr;
   }
 }
