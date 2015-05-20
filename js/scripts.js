f85e08d792b651e7e677691909bf074cf826e044
Chris Davies <chris.davies@theironyard.com>
2015-05-20 09:20:25 -0400

    Some minor tweaks

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,21 @@
 (function () {
   'use strict';
 
+  // parseCsv(csvString, ['id', 'survived', 'pclass'], true);
+
+  // var records = CSV.toArray(titanic)
+  //   .map(arrayToObject(['id', 'survived', 'pclass', 'name']))
+  //   .slice(1);
+
   var records = CSV.toArray(titanic)
-    .map(arrayToObject(['id', 'survived', 'pclass', 'name']))
+    .map(function (columns) {
+      return {
+        id: columns[0],
+        survived: columns[1],
+        pclass: columns[2],
+        name: columns[3]
+      }
+    })
     .slice(1);
 
   console.log(records);
